37d049e45619729aa10d63db48f3916a6b1308b8
AEMIR123 <smx2979@gmail.com>
2023-10-21 11:21:43 +0200

    CalcSalary

--- CalcSalary/src/CalcSalary.java
+++ CalcSalary/src/CalcSalary.java
@@ -1,23 +1,12 @@
-import java.util.Scanner;
-
 public class CalcSalary {
     public static void main(String[] args){
-        System.out.println("how much do you earn a year");
-        Scanner scan = new Scanner(System.in);
-        int dochodb = scan.nextInt();
-        scan.close();
-        int dochodn;
-        int i = 0;
-        int[] tab = new int[4];
-        for(String arg: args) {
-            tab[i] = Integer.parseInt(arg);
-            i++;
-        }
-        if(dochodb<tab[2]){
-            dochodn = dochodb*(100-tab[0])/100;
+        int incomeNet;
+        int incomeGross = Integer.parseInt(args[0]);
+        if(incomeGross<12000){
+            incomeNet = incomeGross*88/100;
         }else{
-            dochodn = (tab[2]*(100-tab[0])+(dochodb-tab[2])*(100-tab[1]))/100;
+            incomeNet = (12000*88+(incomeGross-12000)*68)/100;
         }
-        System.out.println(dochodn);
+        System.out.println(incomeNet);
     }
 }
